13a156ea1cf05b18891c17bb6351186b04cea067
shaoqiu <xushaoqiu220@gmail.com>
2015-03-08 23:00:03 +0800

    add get int and string config interface

--- src/com/shaoqiu/luajava/LuaState.java
+++ src/com/shaoqiu/luajava/LuaState.java
@@ -3,9 +3,11 @@ package com.shaoqiu.luajava;
 public class LuaState {
     private final static String LUAJAVA_LIB = "luajava";
     private long mLuaStatePtr;
-    public native long _luaL_newstate();
-    public native void _luaL_openlibs(long luaState);
-    public native int _luaL_dofile(long luaState, String file);
+    private native long _luaL_newstate();
+    private native void _luaL_openlibs(long luaState);
+    private native int _luaL_dofile(long luaState, String file);
+    private native int _get_global_integer(long luaState, String config);
+    private native String _get_global_string(long luaState, String config);
     static
     {
         System.loadLibrary(LUAJAVA_LIB);
@@ -20,8 +22,19 @@ public class LuaState {
         mLuaStatePtr = _luaL_newstate();
         _luaL_openlibs(mLuaStatePtr);
     }
+    public int getGlobalInteger(String config) {
+        return _get_global_integer(mLuaStatePtr, config);
+    }
+    public String getGlobalString(String config) {
+        return _get_global_string(mLuaStatePtr, config);
+    }
     public static void main(String[] args) {
         System.out.println("luajava test");
-        new LuaState().doFile("/home/shaoqiu/hello.lua");
+        LuaState lua = new LuaState();
+        lua.doFile("hello.lua");
+        int width = lua.getGlobalInteger("width");
+        System.out.println("width = " + width);
+        String shaoqiu = lua.getGlobalString("shaoqiu");
+        System.out.println("shaoqiu = " + shaoqiu);
     }
 }
---
+++ src/hello.lua
@@ -0,0 +1,6 @@
+width=300
+height=400
+shaoqiu='我本将心向明月，奈何明月照沟渠'
+print("hello lua script")
+print(width)
+print(height)
